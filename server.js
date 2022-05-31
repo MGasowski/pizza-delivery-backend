917539f4d95737c0e9bb60ea7ccf7240076c35a4
MGasowski <gasowski0marcin@gmail.com>
2022-05-31 13:42:55 +0200

    s02pt10 getpizzas API route

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 
-const db = require("./db");
+const Pizza = require("./models/pizzaModel");
 
 const app = express();
+const db = require("./db");
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("sever working" + port);
+});
+
+app.get("/getpizzas", (req, res) => {
+  Pizza.find({}, (err, docs) => {
+    if (err) console.log(err);
+    else res.send(docs);
+  });
 });
 
 const port = process.env.PORT || 5000;
